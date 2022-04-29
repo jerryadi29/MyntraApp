769f6153f2c74b740db48aa9b363fb8d316f46f9
aditya rout <aditrout29@gmail.com>
2022-04-29 18:48:48 +0530

    Nav Component added

---
+++ src/components/Header/Styled/Input.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function Input() {
+  return (
+    <>
+    <input type="search" placeholder='Search for Products and more'></input>
+    </>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
