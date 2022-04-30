ec4a3416f045d1bde9e6354b0ff5d5875e2646da
Mayanklakhina <90982737+Mayanklakhina@users.noreply.github.com>
2022-04-30 12:39:37 +0530

    created a navigation file (#1)
    merged navigation file

---
+++ Downloads/MyntraApp-main/src/components/Header/Styled/Logo/logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import all from "../Logo/logo.css"
+
+export default function Logo(props) {
+
+    
+   
+  return (
+   
+        <div className={props.className} style={all}>
+           <div className='logo-image' >
+
+           </div>
+        </div>
+    
+  )
+}
